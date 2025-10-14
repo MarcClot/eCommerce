e8ad576b06167d3b8a3ac36635b0f152353efcd3
MarcClot <455811.clot@fje.edu>
2025-10-14 17:02:33 +0200

    Server IP modified into system domain name which this allow us to use https without any problem

--- app/cli/reusable.js
+++ app/cli/reusable.js
@@ -1,13 +1,12 @@
 function buildRequest(phpFile) {
-    const ip = "192.168.1.86";
+    const ip = "iot.electronics.cat";
     const folder = "eCommerce/app/srv";
-    return "http://" + ip + "/" + folder + "/" + phpFile;
+    return "https://" + ip + "/" + folder + "/" + phpFile;
 }
 
 function formatPrice(price) {
     return price.toFixed(2) + '€';
 }
-
 function buildFullName(name, lastName, secondLastName) {
     return name + ' ' + lastName + ' ' + secondLastName;
 }
